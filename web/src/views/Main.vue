cc5b7c8f8a8ff71d4882a0bdb2bc6351322ad1f3
wg <18367319791@163.com>
2019-11-25 09:42:38 +0800

    remove something

--- web/src/views/Main.vue
+++ web/src/views/Main.vue
@@ -9,7 +9,7 @@
             <button type="button" class="btn bg-primary">立即下载</button>
         </div>
         <div class=" bg-primary pt-3 pb-2">
-            <div class="nav d-flex text-white jc-around">
+            <div class="nav d-flex text-white jc-around pb-1">
                 <div class="nav-item active">
                     <router-link class="nav-link " tag="div" to="/">首页</router-link>
                 </div>
@@ -22,6 +22,7 @@
 
             </div> 
         </div>
+        <!-- 子容器 -->
          <router-view ></router-view>
     </div>
 </template>
